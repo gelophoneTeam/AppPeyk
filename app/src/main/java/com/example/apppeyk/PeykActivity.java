13c6b01d62b1451b76c650c060758c6441213e51
Farzaneh <fbokharaei1993@gmail.com>
2019-02-07 16:15:53 +0330

    Add First Activity Peyk

---
+++ app/src/main/java/com/example/apppeyk/PeykActivity.java
@@ -0,0 +1,37 @@
+package com.example.apppeyk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.widget.CompoundButton;
+
+public class PeykActivity extends AppCompatActivity {
+
+    private SwitchCompat toggleBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peyk);
+
+        toggleBtn=(SwitchCompat)findViewById(R.id.toggleBtn);
+//        toggleBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (toggleBtn.isChecked()) {
+//
+//                    Intent i=new Intent(SecondPage.this,Available.class);
+//                    startActivity(i);
+//
+//                } else {
+//
+//                    Intent i=new Intent(SecondPage.this,UnAvailable.class);
+//                    startActivity(i);
+//
+//                }
+//            }
+//
+//        });
+    }
+}
